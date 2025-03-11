038bd87c08203ad3f710aab9ae3e8fc77f01cedd
Jim Bennett <jim@pieces.app>
2025-03-11 10:54:27 -0700

    Adding readme and some tidying

--- Program.cs
+++ Program.cs
@@ -6,6 +6,41 @@ using Microsoft.Extensions.AI;
 using Pieces.Extensions.AI;
 using Pieces.OS.Client;
 
+// Define a handler for the tool.
+// This takes a question as a string, and sends it to Pieces
+// This uses the LTM with a 7 day memory
+Func<object, Task<string>> func = static async question =>
+{
+    // Create a connection to PiecesOS
+    PiecesClient client = new();
+
+    // Create a chat client using GPT-4o Mini
+    var model = await client.GetModelByNameAsync("GPT-4o Mini Chat Model").ConfigureAwait(false);
+
+    // Define the chat client, and the options to enable long term memory for 7 days
+    IChatClient chatClient = new PiecesChatClient(client, "Pieces MCP Server", model: model);
+    var chatOptions = new ChatOptions
+    {
+        AdditionalProperties = new AdditionalPropertiesDictionary
+        {
+            { PiecesChatClient.LongTermMemoryPropertyName, true},
+            { PiecesChatClient.LongTermMemoryTimeSpanPropertyName, TimeSpan.FromDays(7)}
+        }
+    };
+
+    // Build a list of chat messages. Every conversation is new, so there is no chat history in the context
+    List<ChatMessage> chatMessages = [
+        new(ChatRole.User, question?.ToString())
+    ];
+
+    // Send the chat messages to Pieces, and get the response
+    var response = await chatClient.GetResponseAsync(chatMessages, options: chatOptions).ConfigureAwait(false);
+
+    // Return the response
+    return response.Choices[0].Text!;
+};
+
+// Register the tool with the server
 MCPServer.AddToolHandler(new Tool()
 {
     Name = "PiecesLTM",
@@ -18,29 +53,7 @@ MCPServer.AddToolHandler(new Tool()
             {"question", new ParameterSchema{Type="string", Description="question to ask Pieces LTM"}}
         }
     }
-}, async (object question) =>
-{
-    // Create a connection to PiecesOS
-    PiecesClient client = new();
+}, func);
 
-    // Create a chat client using GPT-4o Mini
-    var model = await client.GetModelByNameAsync("GPT-4o Mini Chat Model");
-
-    IChatClient chatClient = new PiecesChatClient(client, "Pieces MCP Server", model: model);
-    var chatOptions = new ChatOptions
-    {
-        AdditionalProperties = new AdditionalPropertiesDictionary
-    {
-        { PiecesChatClient.LongTermMemoryPropertyName, true},
-        { PiecesChatClient.LongTermMemoryTimeSpanPropertyName, TimeSpan.FromDays(7)}
-    }
-    };
-
-    List<ChatMessage> chatMessages = [
-        new(ChatRole.User, question?.ToString())
-    ];
-    var response = await chatClient.GetResponseAsync(chatMessages, options: chatOptions);
-    return response.Choices[0].Text;
-});
-
-await MCPServer.StartAsync("PiecesLTM", "1.0.0");
+// Start the server
+await MCPServer.StartAsync("PiecesLTM", "1.0.0").ConfigureAwait(false);
--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# PiecesMCPNet
+# Pieces MCP Net
+
+A C# implementation of [MCP (Model context protocol)](https://modelcontextprotocol.io/introduction) using Pieces Long-Term Memory powered by the [Pieces C# SDK](https://github.com/pieces-app/pieces-os-client-sdk-for-csharp).
+
+This contains an MCP tool that answers questions using the Pieces Long-Term Memory for the past 7 days. This limit is hard coded, but this code can be expanded to make this configurable.
+
+## Setup
+
+This requires:
+
+- .NET 9 SDK installed
+- [Pieces](https://pieces.app) installed, and the LTM engine enabled
+
+## Build the code
+
+It is easiest to build and publish a single binary that you can then call from your MCP server. This project is configured for a self-contained single file output.
+
+To build and publish, use this command:
+
+```bash
+dotnet publish -r <platform>
+```
+
+Replace `<platform>` with the [RID of your platform of choice](https://learn.microsoft.com/dotnet/core/rid-catalog#known-rids).
+
+This will create a single executable in the `Release/net9.0/<platform>/publish/` folder called `PiecesMCPNet`
+
+## Register with Claude for Desktop
+
+To register this with Claude for Desktop, add the following to [your `claude_desktop_config.json` file](https://modelcontextprotocol.io/quickstart/user#2-add-the-filesystem-mcp-server):
+
+```json
+{
+  "mcpServers": {
+    "PiecesLTM": {
+      "command": "<location>PiecesMCPNet"
+    }
+  }
+}
+```
+
+Replace `<location>` with the path to this file.
+
+You can now re-launch Claude for Desktop, and this tool will be registered.
+
+To invoke it, prompt with:
+
+```output
+Ask Pieces LTM <question>
+```
+
+You will need to allow Pieces for the chat or single message in Claude for Desktop.
+
+![A gif of claude answering a question using Pieces](./img/Claude.gif)
